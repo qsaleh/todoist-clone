fa3ad48842eaaf71c7a46b86d28083ca3f1f92bd
Qusai Saleh <qusaisaleh@Candices-MacBook-Pro.local>
2020-04-14 16:42:09 -0600

    created simple header

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {App} from './App';
+import { render } from 'react-dom';
+import { App } from './App';
 
-
+  
 render(
   <React.StrictMode>
     <App />
